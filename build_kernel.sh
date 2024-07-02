cbb33aff8d653d1c6167d634c49d256c87fc9220
ramenrolls <ramenrolls@proton.me>
2024-07-02 11:46:12 +0200

    gta3xlwifi: staging GCC toolchain export

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,7 +3,9 @@
 
 export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
+export ANDROID_VERSION=90000
 export ARCH=arm64
-
-make ARCH=arm64 exynos7885-gta3xlwifi_defconfig
-make ARCH=arm64 -j64
+export CROSS_COMPILE=/home/martin/toolchain/gcc/bin/aarch64-linux-android-
+export PATH=/home/martin/toolchain/gcc/bin/aarch64-linux-android-:$PATH
+make ARCH=arm64 gta_defconfig
+make ARCH=arm64 -j12
